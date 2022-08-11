8675243943a60116a1a4cf354b6ed0d21cb3aeb8
Kocalkocal93Kocalkocal93 <86622926+Kocalkocal93Kocalkocal93@users.noreply.github.com>
2022-08-11 20:34:08 +0700

    Update naon3.sh

--- naon3.sh
+++ naon3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+mkdir kemon
+cd kemon
 apt-get install wget -y
 wget https://github.com/Kocalkocal93Kocalkocal93/tuyul.sh/raw/main/abc.tar.gz
 tar -xvf abc.tar.gz
-cd 1.34a
+cd kemon/1.34a
 ./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.bius3 --ethstratum ETHPROXY
